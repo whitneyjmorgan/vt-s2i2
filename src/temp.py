d7658e36591eeba593b58e667d9ae921ba512be5
whitneyjmorgan <whitneyjmorgan@gmail.com>
2015-06-23 10:00:14 -0400

    final temp.py

--- src/temp.py
+++ src/temp.py
@@ -10,5 +10,14 @@ lol
 def f_to_k(F):
 	K = ((F-32.0)*(5.0/9.0)+273.15)
 	return K
-print(f_to_k(5))
+def k_to_c(F):
+	return F - 273.15
+def f_to_c(F):
+	temp_k = f_to_k(F)
+	result = k_to_c(temp_k)
+	return result
+print(f_to_k(32))
+print (f_to_k(212))
+print (k_to_c(273.15))
+print (f_to_c(32))
 #woo
